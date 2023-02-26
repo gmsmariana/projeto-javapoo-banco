104054036d4ea9934095a2c4499187fb68777d0f
gmsmariana <marianasince92@gmail.com>
2023-02-26 15:20:59 +0000

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente mariana = new Cliente();
+        mariana.setNome("mariana");
+
+        Conta cc = new ContaCorrente(mariana);
+        cc.depositar(100);
+
+        Conta cp = new ContaPoupanca(mariana);
+        cc.transferir(100, cp);
+
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+    }
 }
